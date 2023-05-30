4581cd3f5ffba03e2a8ea105daa8230a9b63a07c
Wilson R <47545657+pwilsonrf@users.noreply.github.com>
2023-05-30 14:52:57 -0700

    added logo and nav bar place holders to page

--- src/initialLoadFunction.js
+++ src/initialLoadFunction.js
@@ -5,8 +5,70 @@ export function addHomePage (){
     const mainContainer = document.getElementById('content');
     // mainContainer.innerText = "hello";
 
-
+    //Header
     const header = document.createElement('div');
+
+    //Add logo container to header
+    const logo = document.createElement('div');
+    logo.id = "logo-container";
+    
+    //Add nav bar to header
+    const navBar = document.createElement('div');
+    navBar.id = "navbar";
+
+    //Add links to nav bar
+    const aboutUsCont = document.createElement('div');
+    aboutUsCont.className = "nav-item-container";
+
+    const aboutUs = document.createElement('a');
+    aboutUs.innerText = "About Us";
+    aboutUs.className = "nav-item";
+    aboutUsCont.appendChild(aboutUs);
+
+    const successStoriesCont = document.createElement('div');
+    successStoriesCont.className = "nav-item-container";
+
+    const successStories = document.createElement('a');
+    successStories.innerText = "Success Stories";
+    successStories.className = "nav-item";
+    successStoriesCont.appendChild(successStories);
+
+    const categoriesCont = document.createElement('div');
+    categoriesCont.className = "nav-item-container";
+
+    const categories = document.createElement('a');
+    categories.innerText = "Categories";
+    categories.className = "nav-item";
+    categoriesCont.appendChild(categories);
+
+    const pricingCont = document.createElement('div');
+    pricingCont.className = "nav-item-container";
+    
+    const pricing = document.createElement('a');
+    pricing.innerText = "Pricing";
+    pricing.className = "nav-item";
+    pricingCont.appendChild(pricing);
+
+    const getStartedCont = document.createElement('div');
+    getStartedCont.className = "nav-item-container";
+    
+    const getStarted = document.createElement('a');
+    getStarted.innerText = "Get Started";
+    getStarted.className = "nav-item";
+    getStartedCont.appendChild(getStarted);
+
+    //Append Nav Bar childs
+    navBar.appendChild(aboutUsCont);
+    navBar.appendChild(successStoriesCont);
+    navBar.appendChild(categoriesCont);
+    navBar.appendChild(pricingCont);
+    navBar.appendChild(getStartedCont);
+
+    //Append logo and nav bar to header
+    header.appendChild(logo);
+    header.appendChild(navBar);
+    
+    
     header.className = "header";
     mainContainer.appendChild(header);
 

3b4787980d25060be833305af6a62541e727b514
Wilson R <47545657+pwilsonrf@users.noreply.github.com>
2023-05-31 14:56:21 -0700

    Restructuring of code to make create elements more efficient

--- src/initialLoadFunction.js
+++ src/initialLoadFunction.js
@@ -16,46 +16,41 @@ export function addHomePage (){
     const navBar = document.createElement('div');
     navBar.id = "navbar";
 
+
+    const createElement = (element, text = "", parent, className = "") => {
+        const newElement = document.createElement(element);
+        newElement.innerText = text;
+        newElement.className = className;
+        parent.appendChild(newElement);
+    }
+
     //Add links to nav bar
     const aboutUsCont = document.createElement('div');
     aboutUsCont.className = "nav-item-container";
 
-    const aboutUs = document.createElement('a');
-    aboutUs.innerText = "About Us";
-    aboutUs.className = "nav-item";
-    aboutUsCont.appendChild(aboutUs);
-
+    // const aboutUsCont = createElement('div', '', navBar, 'nav-item', '');
     const successStoriesCont = document.createElement('div');
     successStoriesCont.className = "nav-item-container";
-
-    const successStories = document.createElement('a');
-    successStories.innerText = "Success Stories";
-    successStories.className = "nav-item";
-    successStoriesCont.appendChild(successStories);
 
     const categoriesCont = document.createElement('div');
     categoriesCont.className = "nav-item-container";
 
-    const categories = document.createElement('a');
-    categories.innerText = "Categories";
-    categories.className = "nav-item";
-    categoriesCont.appendChild(categories);
-
     const pricingCont = document.createElement('div');
     pricingCont.className = "nav-item-container";
-    
-    const pricing = document.createElement('a');
-    pricing.innerText = "Pricing";
-    pricing.className = "nav-item";
-    pricingCont.appendChild(pricing);
 
     const getStartedCont = document.createElement('div');
     getStartedCont.className = "nav-item-container";
     
-    const getStarted = document.createElement('a');
-    getStarted.innerText = "Get Started";
-    getStarted.className = "nav-item";
-    getStartedCont.appendChild(getStarted);
+
+    
+
+   
+    createElement('a', 'Categories', categoriesCont, 'nav-item', '');
+    createElement('a', 'Success Stores', successStoriesCont, 'nav-item', '');
+    createElement('a', 'Pricing', pricingCont, 'nav-item', '');
+    createElement('a', 'About Us', aboutUsCont, 'nav-item', '');
+    createElement('a', 'Get Started', getStartedCont, 'nav-item', '');
+    
 
     //Append Nav Bar childs
     navBar.appendChild(aboutUsCont);
@@ -72,9 +67,50 @@ export function addHomePage (){
     header.className = "header";
     mainContainer.appendChild(header);
 
+    //Section 1 Content
     const section1 = document.createElement('div');
     section1.className = "section1";
     mainContainer.appendChild(section1);
+
+    //Section 1 Content - LEFT
+    const section1left = document.createElement('div');
+    const section1h1= document.createElement('h1');
+    section1h1.innerText = "Pass All of Your Civil Engineering classes with A";
+
+    const section1P= document.createElement('p');
+    section1P.innerText = "CivETech helps our students to gain the skills and knowledge to excel in every assignment and exams in all Civil Engineering subjects";
+
+    const section1ButtonContainer = document.createElement('div');
+    section1ButtonContainer.id = "Section1-Button-Container"
+    const section1Button1 = document.createElement('button');
+    section1Button1.id = "Section1-Button1";
+    section1Button1.innerText = "Get started";
+
+    const section1Button2 = document.createElement('button');
+    section1Button2.innerText = "Learn more";
+    section1Button2.id = "Section1-Button2";
+
+    section1ButtonContainer.appendChild(section1Button1);
+    section1ButtonContainer.appendChild(section1Button2);
+
+    section1left.appendChild(section1h1);
+    section1left.appendChild(section1P);
+    section1left.appendChild(section1ButtonContainer);
+
+   
+
+    //Section 1 Content - RIGHT
+    const section1right = document.createElement('div');
+    const section1img = document.createElement('img');
+    section1img.src = "/src/img/wilson-main-image.png";
+    section1img.id = "section1-img";
+    section1right.appendChild(section1img);
+
+    section1.appendChild(section1left);
+    section1.appendChild(section1right);
+
+
+    
 
     const section2 = document.createElement('div');
     section2.className = "section2";
